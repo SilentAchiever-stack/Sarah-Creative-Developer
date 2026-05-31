f754b3af58516eaf441f28caff29bfd4b8c2a7a4
olatunji sarah morenikeji <olatunjisarahmorenikeji@gmail.com>
2026-05-31 14:41:00 +0100

    Disable video loading on window load
    Comment out video loading logic on window load event.

--- silent.js
+++ silent.js
@@ -47,9 +47,9 @@ btn.onclick = () => {
     menu.classList.toggle("active");
 };
 
-window.addEventListener('load',()=>{
+/* window.addEventListener('load',()=>{
     const myvideo = document.querySelector('front');
     myvideo.play().catch( error =>{
         console.log('video not loading properly')
     })
-})
+}) */
